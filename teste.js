631397680484c3be11511ef9009fa71331a415b6
Manoel-1962 <manoeljoselm@hotmail.com>
2025-09-05 15:50:34 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,49 @@
+
+
+const gato = {
+    nome:"mimi",
+    idade:"4 anos",
+    raca:"Frajola",
+    especie:"Gato",
+    
+
+    imprimirNomeGato(){
+        console.log(this.nome)
+    }
+}
+
+console.log (gato)
+
+
+
+const cachorro = {
+    nome: "whiski",
+    idade: "2 anos",
+    raca: "vira lata",
+    especie: "cao",
+    consulta: [ "15/09/25", "17/09/25", "15/10/25" ],
+    dono: {nome: "Manoel", celular:"2710-1869"} ,
+    imprimirNomeDono(){
+        console.log(this.dono.nome);
+    },
+    verificarConsulta() {
+        if(this.consulta.length>0){
+            console.log(this.consulta);
+        } else {
+            console.log("Não tem consultas!")
+        }
+    },
+    imprimirIdade(){
+
+        console.log(this.idade)
+    }
+
+    
+
+}
+
+
+cachorro.imprimirNomeDono();
+cachorro.verificarConsulta();
+cachorro.imprimirIdade();
+gato.imprimirNomeGato();
